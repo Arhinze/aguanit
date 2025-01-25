c31de1342407db33cb6c8350e170ea247216de8e
Arinze <francisokoye48@gmail.com>
2025-01-25 20:55:52 +0100

    testing...

--- views/mine.php
+++ views/mine.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php") or die("can't do that");
 
 Index_Segments::header(); 
 
