2ac2bd167e1db5ffa9e6869c70ecc711aa68fcc7
Arinze <francisokoye48@gmail.com>
2025-01-25 21:18:50 +0100

    testing..

--- mining_page/index.php
+++ mining_page/index.php
@@ -1,4 +1,5 @@
 <?php
-include_once($_SERVER["DOCUMENT_ROOT"]."/views/mine.php")  or die("can't do that");
 
-echo "Hello World";
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/mine.php");
+
+?>
--- views/mine.php
+++ views/mine.php
@@ -1,6 +1,7 @@
 <?php
 
 include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+echo "Hello World....";
 
 Index_Segments::header(); 
 
