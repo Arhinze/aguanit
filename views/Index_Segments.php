01a9bc94193708f0fd7a75b0ef088772195a80b3
Arinze <francisokoye48@gmail.com>
2025-01-25 18:44:34 +0100

    spaced out fa links and edited the youtube logo to a more stylish one

--- views/Index_Segments.php
+++ views/Index_Segments.php
@@ -181,7 +181,7 @@ class Index_Segments{
 
             <div class="footer">
                 <div class="footer_fa_links">
-                    <a href="https://youtube.com/@aguanittoken?si=2UPwkGxROq7WpLAO"><i class="fa fa-youtube"></i></a>
+                    <a href="https://youtube.com/@aguanittoken?si=2UPwkGxROq7WpLAO"><i class="fa fa-youtube-play"></i></a>
                     <a href="https://www.facebook.com/profile.php?id=61553828145828"><i class="fa fa-facebook"></i></a>
                     <a href="https://t.me/aguanit"><i class="fa fa-telegram"></i></a>
                     <a href="https://x.com/AguanitToken?t=VwVfUCIOP1xBa9KRrfPHkg&s=09"><i class="fa fa-twitter"></i></a>
