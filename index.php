4e0a8d0ec3b32c9df370bc32956c829668933193
Arinze <francisokoye48@gmail.com>
2025-01-25 21:42:03 +0100

    testing..

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");
+include_once($_SERVER["DOCUMENT_ROOT"]."/views/Index_Segments.php");echo $_SERVER["DOCUMENT_ROOT"];
 
 Index_Segments::header(); 
 ?>
