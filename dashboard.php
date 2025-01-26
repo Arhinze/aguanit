5f7528ca6cc064cfc5577166836f8d527d49940f
Arinze <francisokoye48@gmail.com>
2025-01-26 07:15:58 +0100

    testings

--- dashboard.php
+++ dashboard.php
@@ -52,7 +52,10 @@ include_once("/home/u590828029/domains/aguanit.com/public_html/views/Dashboard_S
     <input type="hidden" name="f_password" value=""/>
 </form>
 -->
+
+<br /><br /><br /><br /><br /><br />
 <a href="https://mine.aguanit.com" style="background-color:green;padding:45px;border-radius:100px;color:#000">Start Mining</a>
+<br /><br /><br /><br /><br /><br />
 
 <!--Start Mining Form ends--> 
 
