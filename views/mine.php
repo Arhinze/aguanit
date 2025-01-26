d8671e3f6dd64d6e0e5c46f1149891a2c2fc2a4a
Arinze <francisokoye48@gmail.com>
2025-01-26 07:11:43 +0100

    testings

--- views/mine.php
+++ views/mine.php
@@ -1,18 +1,22 @@
 <?php
 
 include_once("/home/u590828029/domains/aguanit.com/public_html/views/Dashboard_Segments.php");
+
+Dashboard_Segments::header(); 
+echo "<br /><br /><br /><br /><br /><br />Everything goes here...<br /><br /><br /><br /><br /><br />";
+Dashboard_Segments::dashboard_footer(); 
  
-if(($data) || ((isset($_POST["username_or_email"])) && ((isset($_POST["password"]))))) {
-    Dashboard_Segments::header(); 
-    echo "<br /><br /><br /><br /><br /><br />Everything goes here...<br /><br /><br /><br /><br /><br />";
-    Dashboard_Segments::dashboard_footer(); 
+//if(($data) || ((isset($_POST["username_or_email"])) && ((isset($_POST["password"]))))) {
+//    Dashboard_Segments::header(); 
+//    echo "<br /><br /><br /><br /><br /><br />Everything goes here...<br /><br /><br /><br /><br /><br />";
+//    Dashboard_Segments::dashboard_footer(); 
 
     //if ((isset($_POST["username_or_email"])) && ((isset($_POST["password"])))) {
 
     //}
-} else {
-    header("location:$site_url/login");
+//} else {
+//    header("location:$site_url/login");
     //echo "Data no dey here o";
-}
+//}
 
 ?>
