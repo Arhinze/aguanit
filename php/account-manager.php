a061c16a5edec85ef30c8e9ff57ed1b4d791d169
Arinze <francisokoye48@gmail.com>
2025-01-26 03:35:09 +0100

    testing..

--- php/account-manager.php
+++ php/account-manager.php
@@ -8,7 +8,7 @@ if(isset($_COOKIE["username_or_email"]) && ((isset($_COOKIE["password"])))){
     $user_id = $_COOKIE["username_or_email"];
     $password = $_COOKIE["password"];
 
-    $stmt = $pdo->prepare("SELECT * FROM miners WHERE (username = ? or user_email = ?) AND `password` = ?");
+    $stmt = $pdo->prepare("SELECT * FROM miners WHERE (username = ? OR user_email = ?) AND `password` = ?");
     $stmt->execute([$user_id, $user_id, $password]);
     
     $data = $stmt->fetch(PDO::FETCH_OBJ);
