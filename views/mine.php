4c8f26a16693dd19b93b90b76304afe2593cdf00
Arinze <francisokoye48@gmail.com>
2025-01-26 07:05:08 +0100

    testings

--- views/mine.php
+++ views/mine.php
@@ -7,9 +7,9 @@ if(($data) || ((isset($_POST["username_or_email"])) && ((isset($_POST["password"
     echo "<br /><br /><br /><br /><br /><br />Everything goes here...<br /><br /><br /><br /><br /><br />";
     Dashboard_Segments::dashboard_footer(); 
 
-    if ((isset($_POST["username_or_email"])) && ((isset($_POST["password"])))) {
+    //if ((isset($_POST["username_or_email"])) && ((isset($_POST["password"])))) {
 
-    }
+    //}
 } else {
     header("location:$site_url/login");
     //echo "Data no dey here o";
