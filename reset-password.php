812ce91abab198cab35a0b7b0dc2e262f110a571
Arinze <francisokoye48@gmail.com>
2025-01-28 01:50:36 +0100

    changed 'Login' text color after successful password reset

--- reset-password.php
+++ reset-password.php
@@ -170,7 +170,7 @@ if (isset($_POST["code"])) {
             <div id="message_success" class="message_success" style="display:block">     
                 <div class="clear">
                     <span class="float:right"><b>Password Updated successfully.<br />
-                       Proceed to <a href="/login">Login</a></b></span>
+                       Proceed to <a href="/login" style="color:#042c06">Login</a></b></span>
                     &nbsp;&nbsp;&nbsp;
 
                     <i class="fa fa-times" style="float:right" onclick="show_div('message_success')"></i>
