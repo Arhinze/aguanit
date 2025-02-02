cad3a400847cfdffdac6935afa1a8605c2e7ae5e
Arinze <francisokoye48@gmail.com>
2025-02-02 10:27:37 +0100

    making mining page active

--- views/Dashboard_Segments.php
+++ views/Dashboard_Segments.php
@@ -116,7 +116,7 @@ HTML;
             }
 
             function show_class_div() {
-                //const collection = document.getElementsByClassName("invalid");
+                const collection = document.getElementsByClassName("invalid");
                 i = 0;
 
                 for (i=0; i<collection.length; i++){
