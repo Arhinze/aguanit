85626f4163a0cf1c8557a77382123dd7160f19b4
Arinze <francisokoye48@gmail.com>
2025-02-02 12:08:47 +0100

    making mining page active

--- views/mine.php
+++ views/mine.php
@@ -37,7 +37,7 @@ if ($data){//$data from account-manager.php
         
         <div id="ajax_mine"></div>
 
-        <div style="font-size:30px;font-weight:bold;font-family:Arial;margin-bottom:30px"><img src="/home/u590828029/domains/aguanit.com/public_html/static/images/logo.png" style="width:30px;height:30px"/><span id="amount_mined"><?=$amount_mined?></span></div>
+        <div style="font-size:30px;font-weight:bold;font-family:Arial;margin-bottom:30px"><img src="<?=$site_url?>/static/images/logo.png" style="width:30px;height:30px"/><span id="amount_mined"><?=$amount_mined?></span></div>
 
         <button class="mining_button" onclick="start_mining(u_name='<?=$data->username?>', u_password='<?=$data->password?>')" style="height:180px;width:180px;background-color:#0bee3ccc;border-radius:600px;text-align:center;color:#fff;font-weight:bold">Click me to start mining</button>
     </center>
