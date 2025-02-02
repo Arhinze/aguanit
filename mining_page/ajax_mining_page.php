8b16662d53330a5b9f918ac2331edaa2707e5a62
Arinze <francisokoye48@gmail.com>
2025-02-02 09:28:47 +0100

    making mining page active

--- mining_page/ajax_mining_page.php
+++ mining_page/ajax_mining_page.php
@@ -37,17 +37,19 @@ if((isset($_GET["un"])) && ((isset($_GET["up"])))){
         
     if ($data) {
         if ($data->mining_status != "active"){
-            $sel_upd_stmt = $pdo->prepare("SELECT * FROM miners WHERE username = ?");
-            $sel_upd_stmt->execute([$data->username]);
+            //$sel_upd_stmt = $pdo->prepare("SELECT * FROM miners WHERE username = ?");
+            //$sel_upd_stmt->execute([$data->username]);
 
-            $sel_upd_data = $sel_upd_stmt->fetch(PDO::FETCH_OBJ);
+            //$sel_upd_data = $sel_upd_stmt->fetch(PDO::FETCH_OBJ);
 
-            if ($sel_upd_data) {
-                echo "<br /><br /><br /><br /><br /><br /> User Exists".$data->mining_status;
-            }
+            //if ($sel_upd_data) {
+            //    echo "<br /><br /><br /><br /><br /><br /> User Exists".$data->mining_status;
+            //}
 
             $update_stmt = $pdo->prepare("UPDATE miners SET mining_status = ? AND mining_start_time = ? WHERE `username` = ?");
             $update_stmt->execute(["active", date("Y-m-d h:i:s", time()), $data->username]);
+
+            echo "<br /><br /><br /><br /><br /><br /> User Exists - ".$data->mining_status." ".$data->mining_start_time." ".$data->username;
 
             echo "<br /><br /><br /><br /><br /><br /><br /><br />Congrats, you've successfully initiated the mining cycle.";
         } else {
