ffd63e2926b092444594e47a29d8b114325bfa1e
Arinze <francisokoye48@gmail.com>
2025-02-02 10:01:59 +0100

    making mining page active

--- php/account-manager.php
+++ php/account-manager.php
@@ -12,9 +12,7 @@ if((isset($_COOKIE["username_or_email"])) && ((isset($_COOKIE["password"])))){
     $stmt->execute([$user_id, $user_id, $password]);
     
     $data = $stmt->fetch(PDO::FETCH_OBJ);
-        
-    //} else {
-    //    header("location:$site_url/login");
+
 }
 
 // then call 'if data(){ ... }' for all necessary dashboard related page.
