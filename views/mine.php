c29a4a751ce2869ebf6d0a23681d390695d2f1bd
Arinze <francisokoye48@gmail.com>
2025-02-02 12:31:04 +0100

    making mining page active

--- views/mine.php
+++ views/mine.php
@@ -43,7 +43,7 @@ if ($data){//$data from account-manager.php
         </div>
 
         <button class="mining_button" onclick="start_mining(u_name='<?=$data->username?>', u_password='<?=$data->password?>')" style="height:180px;width:180px;background-color:#0bee3ccc;border-radius:600px;text-align:center;color:#fff;font-weight:bold">
-            <i class="fa fa-power-off" style="font-size:105px"></i>
+            <i class="fa fa-power-off" style="" id="inner_button"></i>
         </button>
     </center>
     </div>
