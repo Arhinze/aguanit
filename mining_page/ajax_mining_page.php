025b48ef839c5ee8404e6d2f7062da91588139db
Arinze <francisokoye48@gmail.com>
2025-02-02 06:33:21 +0100

    making mining page active

--- mining_page/ajax_mining_page.php
+++ mining_page/ajax_mining_page.php
@@ -1,8 +1,8 @@
 <?php
 
 if((isset($_GET["un"])) && ((isset($_GET["up"])))){
-    $user_id = $_GET["un"];
-    $password = $_GET["up"];
+    $user_id = htmlentities($_GET["un"]);
+    $password = htmlentities($_GET["up"]);
 
     $stmt = $pdo->prepare("SELECT * FROM miners WHERE (username = ? OR user_email = ?) AND `password` = ?");
     $stmt->execute([$user_id, $user_id, $password]);
@@ -10,7 +10,7 @@ if((isset($_GET["un"])) && ((isset($_GET["up"])))){
     $data = $stmt->fetch(PDO::FETCH_OBJ);
         
     if ($data) {
-        if ($data->status == "inactive") {
+        if ($data->mining_status == "inactive") {
             $update_stmt = $pdo->prepare("UPDATE miners SET mining_start_time = ? AND mining_status = ?");
             $stmt->execute([date("Y-m-d h:i:s", time()), "active"]);
 
