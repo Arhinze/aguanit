8c3a9743a59ec813c0cd143cea62904e048247a3
Arinze <francisokoye48@gmail.com>
2025-02-02 13:16:19 +0100

    making mining page active

--- dashboard.php
+++ dashboard.php
@@ -14,38 +14,32 @@ include_once("/home/u590828029/domains/aguanit.com/public_html/views/Dashboard_S
         
 ?>
 
-
-<!--Start Mining Form Starts--> 
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-<form method="post" action="<?=$site_mining_page_url?>">
-    <input type="hidden" name="f_username_or_email" value="<?=$form_user_id?>"/>
-    <input type="hidden" name="f_password" value="<?=$form_password?>"/>
-
-    <button type="submit" class="mining_button"><b>Start Mining</b></button>
-</form>
-<!--Start Mining Form ends--> 
-
-<!--
-<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-<a href="https://mine.aguanit.com" style="background-color:green;padding:45px;border-radius:100px;color:#000">Start Mining</a>
-<br /><br /><br />
--->
-
-<!-- Referral Link section starts -->
-<div style="padding:12px">
-    <!-- style="color:#afabab" --><h3 style="color:#fff">Your Referral Link</h3>
-        
-    <input style="height:24px;border:1px solid #2b8eeb;
-        border-right:30px solid #0bee3ccc;width:80%;
-        border-radius:4px;margin-top:8px" id = 'referral_link'
-        value="https://<?=$site_url_short?>/?ref=<?=$_COOKIE['username_or_email']?>"/>
-        
-    <i style="margin-left:-27px" class="fa fa-copy" onclick="copyText('referral_link')"></i>
-    <br /> 
+<div style="margin-top:60px">
+    <center>
+    <!--Start Mining Form Starts--> 
+    <form method="post" action="<?=$site_mining_page_url?>">
+        <input type="hidden" name="f_username_or_email" value="<?=$form_user_id?>"/>
+        <input type="hidden" name="f_password" value="<?=$form_password?>"/>
+    
+        <button type="submit" class="mining_button"><b>Start Mining</b></button>
+    </form>
+    <!--Start Mining Form ends--> 
+    </center>
+    
+    <!-- Referral Link section starts -->
+    <div style="padding:12px">
+        <h3 style="color:#fff">Your Referral Link</h3>
+            
+        <input style="height:24px;border:1px solid #2b8eeb;
+            border-right:30px solid #0bee3ccc;width:80%;
+            border-radius:4px;margin-top:8px" id = 'referral_link'
+            value="https://<?=$site_url_short?>/?ref=<?=$_COOKIE['username_or_email']?>"/>
+            
+        <i style="margin-left:-27px" class="fa fa-copy" onclick="copyText('referral_link')"></i>
+        <br /> 
+    </div>
+    <!-- Referral Link section ends -->
 </div>
-<!-- Referral Link section ends -->
-
-
 
 <?php
 Dashboard_Segments::footer();
