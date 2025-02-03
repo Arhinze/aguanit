ac5264c01deacdb231d32740ca501ccf507218be
Arinze <francisokoye48@gmail.com>
2025-02-04 00:14:27 +0100

    creating and editing smart-contract page

--- smart-contract.php
+++ smart-contract.php
@@ -9,17 +9,17 @@ Index_Segments::header($title="Smart Contract");
     <div class="smart_contract">
         <div style="font-size:24px;font-weight:bold">Smart Contract</div>
         <hr />
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Smart Contract Address:</span> 0x2547e1Cf16266a211EE4C651CB6d30f732A1DFd7</div>
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Smart Contract Address:</span> 0x2547e1Cf16266a211EE4C651CB6d30f732A1DFd7</div>
 
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Network:</span> Avalanche C-Chain</div>
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Network:</span> Avalanche C-Chain</div>
         
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Token Name:</span> <?=$site_name?></div> 
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Token Name:</span> <?=$site_name?></div> 
         
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Symbol:</span> $<?=$token_name?></div>
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Symbol:</span> $<?=$token_name?></div>
         
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Decimals:</span> 18</div>
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Decimals:</span> 18</div>
         
-        <div class="smart_contract_texts"><span class="smart_contract_text_head">Token Tracker:</span> https://snowtrace.io/token/0x2547e1Cf16266a211EE4C651CB6d30f732A1DFd7?chainid=43114</div>
+        <div class="smart_contract_texts"><span class="smart_contract_texts_head">Token Tracker:</span> https://snowtrace.io/token/0x2547e1Cf16266a211EE4C651CB6d30f732A1DFd7?chainid=43114</div>
     </div>
 </div>
 
