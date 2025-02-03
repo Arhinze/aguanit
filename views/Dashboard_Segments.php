f84c3d9049e9122e1498032e4215a64d83d63db5
Arinze <francisokoye48@gmail.com>
2025-02-04 00:07:27 +0100

    creating and editing smart-contract page

--- views/Dashboard_Segments.php
+++ views/Dashboard_Segments.php
@@ -4,7 +4,7 @@ include_once("/home/u590828029/domains/aguanit.com/public_html/php/account-manag
 include_once("/home/u590828029/domains/aguanit.com/public_html/views/Index_Segments.php");
 
 class Dashboard_Segments extends Index_Segments{
-    public static function header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $site_mining_page_url = SITE_MINING_PAGE_URL, $Hi_user = ""){
+    public static function header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $site_mining_page_url = SITE_MINING_PAGE_URL, $Hi_user = "",$title=SITE_NAME){
 
     
     $css_version = filemtime("/home/u590828029/domains/aguanit.com/public_html/static/style.css");
