c21eeb6b5d9d82fc1b5ea41ba1cd299402fc80f8
Arinze <francisokoye48@gmail.com>
2025-02-04 10:52:31 +0100

    adding airdrop feature

--- claim-airdrop.php
+++ claim-airdrop.php
@@ -5,7 +5,7 @@ include_once("/home/u590828029/domains/aguanit.com/public_html/views/Index_Segme
 //if($data) {
     Index_Segments::header($site_name = SITE_NAME_SHORT, $site_url = SITE_URL, $site_mining_page_url = SITE_MINING_PAGE_URL, $Hi_user = "", $title="Claim Airdrop - Aguanit Token");
 ?>
-    <div class = "dashboard_div" style="background-color:none">
+    <div class = "dashboard_div" style="background-color:<?=$site_color_dark?>">
         <input class="input" name="aguat_wallet_address" placeholder = "Enter Your $<?=$token_name?> Wallet Address"/> 
         <div style = "margin:15px 0 9px 0">
             <button class="long-action-button">Claim Airdrop</button>
