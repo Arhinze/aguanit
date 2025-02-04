b2c745f64a0d04608b18117236862bce6df7732b
Arinze <francisokoye48@gmail.com>
2025-02-04 12:22:48 +0100

    adding airdrop feature

--- ajax_claim_airdrop.php
+++ ajax_claim_airdrop.php
@@ -2,14 +2,31 @@
 
 include_once("/home/u590828029/domains/aguanit.com/public_html/views/account-manager.php");
 
-//if($data) {
-echo "<div class='pop_up'>Hwfa</div>";
+if($data) {
+    $aguat_wa = (htmlentities($_GET["aguat_wallet_add"]) !== null) ? htmlentities($_GET["aguat_wallet_add"]) : " ";
+    if ($data->airdrop_status !== "participated") {//user has not participated ~ insert user
+  
+        $air_stmt = $pdo->prepare("UPDATE miners SET aguat_wallet_address = ? WHERE user_id = ?");
+        $air_stmt->execute($aguat_wa, $data->user_id);
+
+        echo "<div class='pop_up'>Congratulations. You've been successfully added to the waitlist. 
+        <span style='float:right;position:absolute;top:6px;right:6px'>
+            <i class='fa fa-times' onclick='close_pop_up()'></i>
+        </span>
+        </div>";
+    } else {//user has already participated ~ update again, because he/she might be trying to change the wallet addrress, then remind him/her that they have participated in the past
+        echo "<div class='pop_up'>Congratulations. You've already been added to the waitlist. 
+        <span style='float:right;position:absolute;top:6px;right:6px'>
+            <i class='fa fa-times' onclick='close_pop_up()'></i>
+        </span>
+        </div>";
+    }  
 ?>
 
 <?php
-//} else {
-    //echo "<div class='pop_up'> Kindly Login or Sign up now to access this feature </div>";
-    //sleep(3000);
-    //header("location:/login");
-//}
+} else {
+    echo "<div class='invalid'>Kindly <b><a href='/login' style='color:#042c06'>Login</a></b> to access this feature. 
+    <span style='float:right;margin:4px 18px'><i class='fa fa-times' onclick='hide_invalid_div()'></i></span>
+    </div>";
+}
 ?>
