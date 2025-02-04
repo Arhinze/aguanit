4b834d93cc5689dc139764adabaa6d8fb49834a9
Arinze <francisokoye48@gmail.com>
2025-02-04 07:43:43 +0100

    bringing admin page to life

--- ajax_search.php
+++ ajax_search.php
@@ -1,13 +1,19 @@
 <?php
 
 include_once($_SERVER["DOCUMENT_ROOT"]."/php/connection.php");
+if(isset($_COOKIE["admin_name"]) && isset($_COOKIE["admin_password"])){
+    $stmt = $pdo->prepare("SELECT * FROM `admin` WHERE admin_name = ? AND admin_password = ?");
+    $stmt->execute([$_COOKIE["admin_name"], $_COOKIE["admin_password"]]);
+
+    $data = $stmt->fetch(PDO::FETCH_OBJ);
+    if($data){
 
 if(isset($_GET["search_query"])){
     if(!empty($_GET["search_query"])){
         $search_q = htmlentities($_GET["search_query"]);
         $page = htmlentities($_GET["page"]);
     
-
+    
     $search_stmt = $pdo->prepare("SELECT * FROM miners WHERE username LIKE ? LIMIT ?, ?");
     $search_stmt->execute(["%$search_q%", 0, 100]);
     $search_data = $search_stmt->fetchAll(PDO::FETCH_OBJ);
@@ -20,5 +26,11 @@ if(isset($_GET["search_query"])){
 <?php 
        }
     }
+}
+    } else {
+        echo "Stop that nonsense !";
+    }
+} else {
+    echo "Stop that nonsense !!!";
 }
 ?>
